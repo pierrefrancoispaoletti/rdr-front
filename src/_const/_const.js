1e8523c879bb1d733664adf049f49a021600b492
pierrefrancoispaoletti <pierre.paoletti1@gmail.com>
2021-07-08 19:01:27 +0200

    first commit

---
+++ src/_const/_const.js
@@ -0,0 +1,7 @@
+//export const $SERVER = "http://localhost:5000";
+
+export const $SERVER = "https://le-rdr.herokuapp.com";
+
+export const GOOGLE_API_KEY = "AIzaSyAZaWwVgn5z9gNPy0cbweEVGUeWwva5GGM";
+
+export const tokenName = "le-RDR"
